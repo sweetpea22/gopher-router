2102bc266aca1bed225e10bb0d1c10819f6708c0
sweetpea22 <cindychau24@me.com>
2023-10-17 12:17:00 -0400

    Add wallet connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
+import BasicTable from './components/Basic'
 import SampleWalletPortfolio from './components/SampleWalletPortfolio'
 import SlideOut from './components/SlideOut'
 
 export default function Home() {
   return (
     <div className="h-full">
-      <SampleWalletPortfolio/>
-      <SlideOut />
+      <BasicTable />
+      {/* <SampleWalletPortfolio/>
+      <SlideOut /> */}
     </div>
   )
 }
