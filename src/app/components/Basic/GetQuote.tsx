d2824fe5e3188362a1b71c3cae400a51b46818fe
sweetpea22 <cindychau24@me.com>
2023-10-17 16:25:30 -0400

    Show cumulative balances crosschain

--- src/app/components/Basic/GetQuote.tsx
+++ src/app/components/Basic/GetQuote.tsx
@@ -3,10 +3,14 @@
 
 import { useAccount, useFeeData } from 'wagmi';
 import { standardButton } from '@/app/styles/styles';
+import InputFields from './InputFields';
+import { getAllBalances } from '@/formulas/utils';
+import { useEffect, useState } from 'react';
+import { formatEther } from 'ethers/lib/utils';
 
 export default function getQuote() {
-  const { data: feeData, isError: feeDataError, isLoading: feeDataLoading } = useFeeData({formatUnits: 'gwei', watch: true})
-  const { address, isConnected } = useAccount();
+  const { data: feeData, isError: feeDataError, isLoading: feeDataLoading } = useFeeData({ formatUnits: 'gwei', watch: true })
+  
 
   const renderLoading = () => {
     if (!feeDataLoading) {
@@ -23,7 +27,7 @@ export default function getQuote() {
     if (!feeData) {
       return (
         <>
-          { renderLoading() }
+          {renderLoading()}
         </>
       )
     }
@@ -34,8 +38,33 @@ export default function getQuote() {
     )
   }
 
+  const { address } = useAccount();
+  const [balances, setBalances] = useState<any>([]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const chains = [{ name: "Goerli", rpcUrl: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161" }, { name: "Sepolia", rpcUrl: "https://sepolia.infura.io/v3/5db0726c373b4e99a389e664e4db0d94" }]
+
+  
+  useEffect(() => {
+    const getBalanceByChain = async () => {
+      const data = await getAllBalances(address as string, chains)
+      setBalances(data);
+    }
+    getBalanceByChain();
+  }, [address, chains])
+
   return (
     <div className='flex flex-col items-center shadow-md p-4 bg-gray-100 rounded-xl'>
+      {/* <InputFields /> */}
+
+      <div className='shadow-lg rounded-xl py-3 px-5'>
+        <h2 className='text-gray-800 '>Cumulative Balance</h2>
+        <div>
+          {balances ? balances.map((item:any, index:number) => (
+            <p className='text-indigo-500' key={index}>{item.chain.name}: <strong>{formatEther(item.balance)} ETH</strong></p>
+          )) : null}
+        </div>
+      </div>
+
       <button
         onClick={() => {console.log('hi')}}
         type='button'
@@ -43,7 +72,7 @@ export default function getQuote() {
         Get quotes
       </button>
       <div className='flex flex-col text-gray-900 text-center items-center my-8 gap-2'>
-        GasPrice:
+        Current Gas Price (Gwei):
         {renderGasPrice()}
       </div>
     </div>
--- src/app/styles/styles.ts
+++ src/app/styles/styles.ts
@@ -1,3 +1,5 @@
 export const standardButton = `relative flex-shrink-0 py-3 px-5 rounded-full text-md bg-indigo-600 text-indigo-100 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-white`
 
 export const centeredDiv = `mx-auto max-w-7xl px-4 sm:px-6 lg:px-8`
+
+export const standardInput = `block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`
