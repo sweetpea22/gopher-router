acd8259cb8433bf364edf259366c067fc265a374
sweetpea22 <cindychau24@me.com>
2023-10-17 12:52:21 -0400

    test wiring

--- src/app/components/Basic/GetQuote.tsx
+++ src/app/components/Basic/GetQuote.tsx
@@ -5,10 +5,10 @@ import { useAccount, useFeeData } from 'wagmi';
 import { standardButton } from '@/app/styles/styles';
 
 export default function getQuote() {
-   const { data: feeData, isError: feeDataError, isLoading: feeDataLoading } = useFeeData()
-  // const [transfers, setTransfers] = useState<Transfer[]>([])
+  const { data: feeData, isError: feeDataError, isLoading: feeDataLoading } = useFeeData()
   const { address, isConnected } = useAccount();
-    const renderLoading = () => {
+
+  const renderLoading = () => {
     if (!feeDataLoading) {
       return null;
     }
@@ -19,26 +19,11 @@ export default function getQuote() {
     )
   }
 
-  // only use this 
-   const renderSuccess = () => {
-    if (!feeData) {
-      return null;
-    }
-    return (
-      <div className="flex flex-col items-center">
-        <div>
-          Proof successfully submitted to Axiom. Proof can take 1-3 minutes to generate...
-        </div>
-      </div>
-    )
-  }
-
   const renderQuotes = () => {
     if (!feeData) {
       return (
         <>
           { renderLoading() }
-          { renderSuccess() }
         </>
       )
     }
@@ -54,11 +39,11 @@ export default function getQuote() {
       <button
         onClick={() => {console.log('hi')}}
         type='button'
-        className={`${standardButton} my-5`}>
+        className={`${standardButton} w-[150px] my-5`}>
         Get quotes
       </button>
       <div className=' flex flex-col text-gray-900 text-center items-center my-8 gap-2'>
-          GasPrice:
+        GasPrice:
       {renderQuotes()}
       </div>
     </div>
