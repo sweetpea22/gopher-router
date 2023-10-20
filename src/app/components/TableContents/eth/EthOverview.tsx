299c98f92eb5a9b0990f4cd5ed74e6d6acf99105
sweetpea22 <cindychau24@me.com>
2023-10-19 22:31:10 -0400

    Add axelar to gasCosts.ts

--- src/app/components/TableContents/eth/EthOverview.tsx
+++ src/app/components/TableContents/eth/EthOverview.tsx
@@ -7,7 +7,6 @@ import { formatEther } from 'ethers/lib/utils';
 import { Chains } from '@/app/constants';
 import { BigNumber } from 'ethers';
 import { AccountDetails } from '@/app/interfaces';
-import { getFee } from '@/formulas/bridges/axelar/axelar';
 
 export function EthOverview() {
   const { address } = useAccount();
@@ -20,7 +19,6 @@ export function EthOverview() {
       setBalances(data);
     }
     getBalanceByChain();
-    getFee('goerli', 'optimism');
   }, [address, Chains])
 
   return (
