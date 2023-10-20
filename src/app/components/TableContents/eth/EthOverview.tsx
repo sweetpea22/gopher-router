e1da197c5e0467dfafd19dd0e2f4093b89958793
sweetpea22 <cindychau24@me.com>
2023-10-20 18:55:07 -0400

    Update UI

--- src/app/components/TableContents/eth/EthOverview.tsx
+++ src/app/components/TableContents/eth/EthOverview.tsx
@@ -2,37 +2,41 @@
 import { useAccount } from 'wagmi';
 import InputFields from './InputFields';
 import { getAllBalances } from '@/formulas/utils';
-import { useEffect, useState } from 'react';
-import { formatEther } from 'ethers/lib/utils';
+import { useEffect, useState, useCallback } from 'react';
+import { computeAddress, formatEther } from 'ethers/lib/utils';
 import { Chains } from '@/app/constants';
 import { BigNumber } from 'ethers';
 import { AccountDetails } from '@/app/interfaces';
+import { Disclosure } from '@headlessui/react';
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+import Button from '../../Button';
+import { standardButton } from '@/app/styles/styles';
 
 export function EthOverview() {
   const { address } = useAccount();
   const [balances, setBalances] = useState<AccountDetails[]>([]);
   const [totalEth, setTotalEth] = useState<Number | any>(0);
 
+  const getBalanceByChain = useCallback(async () => {
+    const data = await getAllBalances(address as string, Chains)
+    setBalances(data);
+  }, [address])
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    const getBalanceByChain = async () => {
-      const data = await getAllBalances(address as string, Chains)
-      setBalances(data);
-    }
-    getBalanceByChain();
+    // getBalanceByChain()
+    // getTotalEth()
+  }, [address, getBalanceByChain])
 
-    const getTotalEth = () => {
-      if (balances.length > 1) {
-      let sum = (a: any[]) => a.reduce((x: any, y: any) => x + y);
-    
-      let totalAmount = sum(balances.map((x) => Number(formatEther(x.balance))));
-        setTotalEth(totalAmount)
-      }
-    }
-    getTotalEth()
-  }, [address, balances, totalEth])
 
+  const getTotalEth = () => {
+    if (balances.length > 1) {
+    let sum = (a: any[]) => a.reduce((x: any, y: any) => x + y);
   
+    let totalAmount = sum(balances.map((x) => Number(formatEther(x.balance))));
+      setTotalEth(totalAmount)
+    }
+  }
 
   return (
     <tr>
@@ -40,19 +44,43 @@ export function EthOverview() {
         <div className='flex flex-row items-center'>
           <div className='bg-indigo-200 rounded-xl h-6 w-6 mr-2'></div>Ethereum
         </div>
-        <div>
+        <div className='flex flex-row'>
            {balances ? balances.map((item:any, index:number) => (
-              <p key={index} className='mt-2 text-gray-200'>{item.chain.name}</p>
+             <p key={index} className='mt-2 mr-2 text-gray-200'>
+              
+               {item.chain.name.toLowerCase()}{index === balances.length - 1 ? null : ', '} </p>
             )) : null}
         </div>
-        </td>
-        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">$1,323 USD</td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{totalEth}</td>
-        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-          <a href="#" className="text-indigo-100 hover:text-indigo-400">
-            Send
-          </a>
-        </td>
+      </td>
+      <td className="whitespace-nowrap text-sm text-gray-300">$1,323 USD</td>
+      <td className="whitespace-nowrap text-sm text-gray-300">{totalEth}</td>
+      <td className="relative whitespace-nowrap py-4 text-right text-sm">
+        <Disclosure as='div'>
+          {({ open }) => (
+            <>
+              <dt>
+                <Disclosure.Button className='flex w-full items-center justify-center text-left text-gray-200'>
+                  <div>
+                    Send 
+                  </div>
+                  <span className='ml-6 flex h-7 items-center'>
+                    {open ? (
+                      <ChevronUpIcon className='h-6 w-6 text-gray-600' aria-hidden='true' />
+                    ) : (
+                      <ChevronDownIcon className='h-6 w-6 text-brand-400' aria-hidden='true' />
+                    )}
+                  </span>
+                </Disclosure.Button>
+              </dt>
+              <Disclosure.Panel as='dd' className='h-[300px]'>
+                <p className='text-sm leading-7 text-gray-400'>
+                  Input fields go in here
+                </p>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </td>
     </tr>
   );
 }
