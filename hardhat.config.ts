f2814bb00e8b508cf849e9323f9954b9885b2ac5
sweetpea22 <cindychau24@me.com>
2023-10-20 18:13:36 -0400

    Add hardhat config + deploy script

---
+++ hardhat.config.ts
@@ -0,0 +1,77 @@
+"use client"
+
+import "dotenv/config"
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "@nomicfoundation/hardhat-verify"
+import "hardhat-deploy"
+import { HardhatUserConfig } from 'hardhat/config';
+
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  'https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY';
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string
+const MANTLE_EXPLORER_API_KEY = process.env.MANTLE_EXPLORER_API_KEY as string;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      gasPrice: 225000000000,
+      chainId: 5,
+    },
+    scroll_sepolia_testnet: {
+      url: "https://rpc.ankr.com/scroll_sepolia_testnet",
+      gasPrice: 1000000000,
+      accounts: [PRIVATE_KEY],
+      chainId: 534351,
+    },
+    mantleTestnet: {
+      url: "https://rpc.testnet.mantle.xyz/",
+      gasPrice: 1000000000,
+      accounts: [PRIVATE_KEY],
+      chainId: 5001,
+    },
+  },
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  //@ts-ignore
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      mantle: MANTLE_EXPLORER_API_KEY,
+
+    },
+  },
+
+  //@ts-ignore
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
+};
+
+export default config;
---
+++ src/scripts/deploy.ts
@@ -0,0 +1,52 @@
+import hre from 'hardhat';
+import verify from './verify';
+
+// read hardhat config to see all chains 
+export interface networkConfigItem {
+  blockConfirmations?: number
+}
+export interface networkConfigInfo {
+  [key: string]: networkConfigItem
+}
+export const networkConfig: networkConfigInfo = {
+  localhost: {},
+  hardhat: {},
+  goerli: {
+    blockConfirmations: 6,
+  },
+  scrollGoerli: { blockConfirmations: 6},
+  gnosis: { blockConfirmations: 6 },
+}
+export const developmentChains = ["hardhat", "localhost"]
+
+async function main() {
+
+  //@ts-ignore
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // add deploy args here
+  const jumpContract = await deploy(
+    '',  {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 6
+  })
+
+    console.log(
+    `Deployed jump contract on  at ${jumpContract.address}!`
+    )
+  
+    // verify on block explorer
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    await verify("0x8162c80C51b2d756E0b9678F5241d6aa71b64805", [{ numberOfLevels: 5 }])
+  }
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
